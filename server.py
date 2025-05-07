b7a7d68c6e4bad42b6f2ea39ab89d80b5e00ab5c
thehanatos <admin4523@gmail.com>
2025-05-07 09:59:58 +0200

    Fix past competitions: [block booking for past competitions]

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask, render_template, request, redirect, flash, url_for
+from datetime import datetime
 
 
 def loadClubs():
@@ -41,15 +42,24 @@ def showSummary():
 
 @app.route('/book/<competition>/<club>')
 def book(competition, club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
-    if foundClub and foundCompetition:
-        return render_template('booking.html',
-                               club=foundClub, competition=foundCompetition)
-    else:
-        flash("Something went wrong-please try again")
-        return render_template('welcome.html',
-                               club=club, competitions=competitions)
+    foundClub = next((c for c in clubs if c['name'] == club), None)
+    foundCompetition = next(
+        (c for c in competitions if c['name'] == competition), None)
+
+    if not foundClub or not foundCompetition:
+        flash("Something went wrong - please try again.")
+        return redirect(url_for('index'))
+
+    # Check if the competition is in the past
+    competition_date = datetime.strptime(
+        foundCompetition['date'], "%Y-%m-%d %H:%M:%S")
+    if competition_date < datetime.now():
+        flash("You cannot book past competitions.")
+        return render_template(
+            'welcome.html', club=foundClub, competitions=competitions)
+
+    return render_template('booking.html',
+                           club=foundClub, competition=foundCompetition)
 
 
 @app.route('/purchasePlaces', methods=['POST'])
